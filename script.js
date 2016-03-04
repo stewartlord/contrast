17768dc5132472b2b3a678f714293ceb2dbc6145
Stewart Lord <stewey@ambitious.ca>
2016-03-03 22:02:49 -0800

    This is a HUGE MESS. Also, a bit of a step backwards.
    However, I think we are on a better track. We now scroll through the
    gutter instead of the left pane. We make the gutter as tall as all of the
    tallest sides of the diff chunks combined. Additionally we maintain an
    index of alignment points (left and right line numbers that should line up).
    The index is keyed on gutter line number. So, as we scroll through the
    gutter we can easily see what line number on the left should be matched
    to which line number on the right for the current scroll position.
    
    Still a ways off making this work. Need to figure out which pane to shift
    up/down for a given alignment point. Need to make it smooth again.
    
    Aaaaand, desperately need to clean up all of the sad sad dead code.

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ path        = require('path');
 var left  = __dirname + '/sample-left.js';
 var right = __dirname + '/sample-right.js';
 
+var alignByLine = [];
 function loadDiff(left, right) {
   $('title').text(path.basename(left) + ' - ' + path.basename(right));
 
@@ -15,14 +16,16 @@ function loadDiff(left, right) {
     loadFile(left,  $('.file-left .file-contents')),
     loadFile(right, $('.file-right .file-contents'))
   ]).then(function(values) {
-    var i          = 0,
-        count      = 0,
-        lineLeft   = 0,
-        lineRight  = 0,
-        isEdit     = false,
-        changes    = diff.diffLines(values[0], values[1]),
-        linesLeft  = $('.file-left  .file-contents div.line'),
-        linesRight = $('.file-right .file-contents div.line');
+    var i           = 0,
+        count       = 0,
+        lineLeft    = 0,
+        lineRight   = 0,
+        isEdit      = false,
+        changes     = diff.diffLines(values[0], values[1]),
+        linesLeft   = $('.file-left  .file-contents div.line'),
+        linesRight  = $('.file-right .file-contents div.line'),
+        chunkSize   = 0;
+        gutterLines = 0;
 
     for (i = 0; i < changes.length; i++) {
       count = changes[i].count;
@@ -35,7 +38,7 @@ function loadDiff(left, right) {
           .addClass('delete').toggleClass('edit', isEdit);
 
         // pair-up left/right changes
-        $(linesLeft.get(lineLeft)).addClass('change change-' + i+ ' line-' + lineLeft);
+        $(linesLeft.get(lineLeft)).addClass('change change-' + i + ' line-' + lineLeft);
         $(linesRight.get(lineRight)).addClass('change-' + i);
 
         // bridge the gap
@@ -63,6 +66,8 @@ function loadDiff(left, right) {
 
       // handle same
       } else {
+        isEdit = false;
+
         // pair-up left/right changes
         $([linesLeft.get(lineLeft), linesRight.get(lineRight)])
           .addClass('change change-' + i);
@@ -71,6 +76,58 @@ function loadDiff(left, right) {
         lineRight += count;
       }
     }
+
+    // make a pass through changes to pair up edits
+    var chunks = [];
+    for (i = 0; i < changes.length; i++) {
+      isEdit = changes[i].removed && i < changes.length && changes[i+1].added;
+
+      chunks.push({
+        edit:   isEdit,
+        add:    isEdit ? false : changes[i].added,
+        delete: isEdit ? false : changes[i].removed,
+        left:   changes[i],
+        right:  isEdit ? changes[i+1] : changes[i],
+        size:   isEdit ? Math.max(changes[i].count, changes[i+1].count) : changes[i].count
+      });
+
+      // skip add half of edits
+      if (isEdit) i++;
+    }
+
+    // if you were to take the tallest side of each diff chunk and stack
+    // them on top of each other, that is how tall we want the gutter to be
+    // that way you can scroll through the gutter and smoothly scroll past
+    // every line in every diff chunk
+    // for a given a 'gutter' line number we want to be able to quickly lookup
+    // the left/right line numbers that we should be trying to align when
+    // scrolled to the top of the chunk vs the bottom of the chunk
+    var chunk       = null,
+        gutterLine  = 0,
+        leftLine    = 0,
+        leftSize    = 0,
+        rightLine   = 0,
+        rightSize   = 0;
+
+    for (i = 0; i < chunks.length; i++) {
+      chunk     = chunks[i];
+      leftSize  = chunk.add    ? 0 : chunk.left.count;
+      rightSize = chunk.delete ? 0 : chunk.right.count;
+
+      var align = {
+        left:  { prev: leftLine,  next: leftLine  + leftSize },
+        right: { prev: rightLine, next: rightLine + rightSize }
+      };
+      for (var j = 0; j < chunk.size; j++) {
+        alignByLine[gutterLine + j] = align;
+      }
+
+      leftLine   += leftSize;
+      rightLine  += rightSize;
+      gutterLine += chunk.size
+    }
+
+    $('.gutter').css('height', (getLineHeight() * gutterLine) + 'px');
   });
 }
 
@@ -119,6 +176,8 @@ function drawBridge(changeLeft, changeRight, lineLeft, lineRight, skew, bridge) 
 
   // adjust lineLeft/Right to account for skew (scroll alignment)
   var lineHeight  = getLineHeight();
+// if (lineHeight !== 18) console.log(lineHeight);
+console.log(skew);
   var action      = changeLeft.removed && changeRight.added ? 'edit' : (changeLeft.removed ? 'delete' : 'add');
   // var offsetLeft  = (lineLeft  - lineRight) * lineHeight;
   // var offsetRight = ((lineRight - lineLeft) * lineHeight) + skew;
@@ -164,7 +223,8 @@ function drawBridge(changeLeft, changeRight, lineLeft, lineRight, skew, bridge) 
 
 var lastSkew = 0;
 function updateBridges() {
-  var skew = parseInt($('.file-right .file-contents').get(0).style.top, 10);
+  var skew = $('.file-right .file-contents').get(0).style.top;
+  skew     = skew ? parseInt(skew, 10) : 0;
   if (skew === lastSkew) return;
 
   $('.bridge').each(function(){
@@ -183,50 +243,97 @@ $(function(){
     var scrollTop   = $(window).scrollTop(),
         lineHeight  = getLineHeight(),
         focalPoint  = Math.floor($(window).height() / 3) + scrollTop,
-        lineNumber  = Math.floor(focalPoint / lineHeight),
-        focalLine   = $('div.line').eq(lineNumber);
+        lineNumber  = Math.floor(focalPoint / lineHeight);
 
-    // now that we have the focal line, use that to find the bracketing changes
-    var prevChange  = focalLine.is('.change') ? focalLine : focalLine.prevAll('.change').first(),
-        nextChange  = focalLine.nextAll('.change').first(),
-        prevTop     = prevChange.offset().top,
-        nextTop     = nextChange.offset().top;
 
-    var prevChangeClass = prevChange.attr('class').match(/change\-[0-9]+/)[0],
-        prevLeftChange  = $('.file-left .'  + prevChangeClass),
-        prevRightChange = $('.file-right .' + prevChangeClass),
-        prevLeftTop     = prevLeftChange.position().top,
-        prevRightTop    = prevRightChange.position().top,
-        prevOffset      = prevLeftTop - prevRightTop;
+    $('.focal-point').css('top', focalPoint);
 
-    var nextChangeClass = nextChange.attr('class').match(/change\-[0-9]+/)[0],
-        nextLeftChange  = $('.file-left .'  + nextChangeClass),
-        nextRightChange = $('.file-right .' + nextChangeClass),
-        nextLeftTop     = nextLeftChange.position().top,
-        nextRightTop    = nextRightChange.position().top,
-        nextOffset      = nextLeftTop - nextRightTop;
-
-    // how far have we scrolled between these two changes in %
-    // snap focal point when within close proximity to a change
-    // !!! doesn't work for adds or edits from small change to big change
-    focalPoint  = focalPoint - prevTop    <= 3 ? prevTop : focalPoint;
-    focalPoint  = nextTop    - focalPoint <= 3 ? nextTop : focalPoint;
-    var percent = (focalPoint - prevTop) / (nextTop - prevTop);
+    var align = alignByLine[lineNumber];
+    $('.file .prev-change, .file .next-change').removeClass('prev-change next-change');
+    $('.file-left  .line:nth-child(' + align.left.prev  + ')').addClass('prev-change');
+    $('.file-left  .line:nth-child(' + align.left.next  + ')').addClass('next-change');
+    $('.file-right .line:nth-child(' + align.right.prev + ')').addClass('prev-change');
+    $('.file-right .line:nth-child(' + align.right.next + ')').addClass('next-change');
 
     // slide between the two offsets using percent to combine them
-    var offset = (prevOffset * (1-percent)) + (nextOffset * percent);
+    var offset = (align.left.prev * lineHeight) - (align.right.prev * lineHeight);
     $('.file-right .file-contents').css('top', offset + 'px');
 
     // redraw connecting svgs
     updateBridges();
 
-    // debug decoration
-    // $('.focal-point').css('top', focalPoint);
-    // $('.focal-line').removeClass('focal-line');
-    // focalLine.addClass('focal-line');
-    // $('.prev-change').removeClass('prev-change');
-    // prevChange.addClass('prev-change');
-    // $('.next-change').removeClass('next-change');
-    // nextChange.addClass('next-change');
+//
+//
+//         focalLine   = $('div.line').eq(lineNumber);
+//
+//   // **************
+//   // the problem at the moment is that we pick focal line based solely on the
+//   // left pane and without accounting for the offset of the left pane
+//
+//     // now that we have the focal line, use that to find the bracketing
+//     // changes and their positions for both the left and right panes
+//     // @todo need to special case scrolling to the top when there are changes
+//     // above the focal line
+//     var prevChange      = focalLine.is('.change') ? focalLine : focalLine.prevAll('.change').first(),
+//         prevChangeClass = prevChange.attr('class').match(/change\-[0-9]+/)[0],
+//         prevLeftChange  = $('.file-left .'  + prevChangeClass),
+//         prevRightChange = $('.file-right .' + prevChangeClass),
+//         prevLeftTop     = prevLeftChange.position().top,
+//         prevRightTop    = prevRightChange.position().top,
+//         prevOffset      = prevLeftTop - prevRightTop;
+//
+//     var nextChange      = focalLine.nextAll('.change').first(),
+//         nextChangeClass = nextChange.attr('class').match(/change\-[0-9]+/)[0],
+//         nextLeftChange  = $('.file-left .'  + nextChangeClass),
+//         nextRightChange = $('.file-right .' + nextChangeClass),
+//         nextLeftTop     = nextLeftChange.position().top,
+//         nextRightTop    = nextRightChange.position().top,
+//         nextOffset      = nextLeftTop - nextRightTop;
+//
+//     // pick the larger of the left vs. right chunk to scroll through
+//     var side = nextLeftTop - prevLeftTop >= nextRightTop - prevRightTop ? 'left' : 'right';
+//
+//     // how far have we scrolled between these two changes in %
+//     var snap = 5;
+//     if (side === 'left') {
+//       focalPoint  = focalPoint  - prevLeftTop <= snap ? prevLeftTop : focalPoint;
+//       focalPoint  = nextLeftTop - focalPoint  <= snap ? nextLeftTop : focalPoint;
+//       var percent = (focalPoint - prevLeftTop) / (nextLeftTop - prevLeftTop);
+//
+//       var offset = (prevOffset * (1-percent)) + (nextOffset * percent);
+//   //    $('.file-right .file-contents').css('top', offset + 'px');
+//       // $('.file-left .file-contents').css('top', '0px');
+//     } else {
+//       focalPoint  = focalPoint   - prevRightTop <= snap ? prevRightTop : focalPoint;
+//       focalPoint  = nextRightTop - focalPoint   <= snap ? nextRightTop : focalPoint;
+//       var percent = (focalPoint  - prevRightTop) / (nextRightTop - prevRightTop);
+//
+//       var offset = (prevOffset * (1-percent)) + (nextOffset * percent);
+// //      $('.file-left .file-contents').css('top', offset + 'px');
+//       // $('.file-right .file-contents').css('top', '0px');
+//     }
+//
+//     // how far have we scrolled between these two changes in %
+//     // snap focal point when within close proximity to a change
+//     // !!! doesn't work for adds or edits from small change to big change
+//     // focalPoint  = focalPoint - prevTop    <= 3 ? prevTop : focalPoint;
+//     // focalPoint  = nextTop    - focalPoint <= 3 ? nextTop : focalPoint;
+//     // var percent = (focalPoint - prevTop) / (nextTop - prevTop);
+//
+//     // slide between the two offsets using percent to combine them
+//     // var offset = (prevOffset * (1-percent)) + (nextOffset * percent);
+//     // $('.file-right .file-contents').css('top', offset + 'px');
+//
+//     // redraw connecting svgs
+//     updateBridges();
+//
+//     // debug decoration
+//     $('.focal-point').css('top', focalPoint);
+//     $('.focal-line').removeClass('focal-line');
+//     focalLine.addClass('focal-line');
+//     $('.prev-change').removeClass('prev-change');
+//     prevChange.addClass('prev-change');
+//     $('.next-change').removeClass('next-change');
+//     nextChange.addClass('next-change');
   });
 });
